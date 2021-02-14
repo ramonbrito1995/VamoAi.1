dfc2c1db2b325c86e5badf158cf3e81ced37f84c
Ramon Brito <62397984+ramonbrito1995@users.noreply.github.com>
2021-02-14 15:03:02 -0300

    Create SituacaoAluno.py

---
+++ Exercicios/SituacaoAluno.py
@@ -0,0 +1,24 @@
+'''Uma professora deseja desenvolver um sistema para automatizar
+seu trabalho. Ela precisa criar uma solução onde seus alunos
+consigam inserir suas notas (seja a média geral de todas as
+matérias ou a média de uma única disciplina), receber a média, e
+saber sua situação (aprovação, reprovação ou recuperação)'''
+
+print("******************************")
+print("Situação do Aluno".center(30))
+print("******************************")
+
+#Programa para calcular a média diretamente e informar a situação do aluno.
+
+def situacao(media):
+    if media >= 7:
+        return "\nAprovado na matéria!\n"
+    elif  media < 7 and media >=4:
+        return 'De recuperação! Para ser aprovado na final, você precisa tirar no mínimo:', 10 - media ,'.'
+    else:
+        return "\nReprovado!\n"
+    
+
+media = float(input("\nDigite a média da matéria: \n"))
+
+print(situacao(media))
